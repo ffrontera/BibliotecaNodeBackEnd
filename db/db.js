eb01f92260384c225625cf73d73a9941cb16b585
ffrontera <ffrontera@live.com>
2024-06-18 11:09:03 -0300

    actualizo db.js para crear la base de datos si no existe

--- db/db.js
+++ db/db.js
@@ -4,15 +4,37 @@ const connection = mysql.createConection({
     host: 'localhost',
     user: 'root',
     password: 'passs',
-    database: 'biblioteca'
 });
 
 connection.conect(error => {
-    if(error) {
-        return console.log(error);
-    }
+    if (error) {
+        return console.error('Error al conectar con db', error);
+    };
 
     console.log('conectado a db');
-})
+
+    connection.query('CREATE DATABASE IF NOT EXISTS biblioteca', (error, results) => {
+        if (error) {
+            return console.error('Error al creal la base de datos', error);
+        }
+        console.log('db asegurada');
+
+        connection.changeUser({ database: 'biblioteca_db' }, (error) => {
+            if (error) {
+                return console.error('Error al cambiar a db biblioteca_db: ', error);
+            }
+
+            //TODO: query para crear todas las tablas de la db
+            const queryCrearTabalas = `
+            `;
+            connection.query(queryCrearTabalas, (error, results) => {
+                if (error) {
+                    return console.error('Error a crear tablas en la db: ', error);
+                }
+                console.log('Tablas aseguradas');
+            });
+        });
+    });
+});
 
 module.exports = connection;
