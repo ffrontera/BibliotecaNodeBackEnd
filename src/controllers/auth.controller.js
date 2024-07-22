ac6b5c52df40eee8c55b3c4c0f03b35d6fb1e0aa
ffrontera <ffrontera@live.com>
2024-07-22 10:30:53 -0300

    test error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const login = async (req, res) => {
         res.json({ auth: true, token, message: 'bienvenido ' + user.nombre });
     } catch (error) {
         if (error) {
-            return res.status(500).json({ 'message': "Intente mas tarde" });
+            return res.status(500).json({ 'message': error });
         }
         if (rows.length === 0) {
             return res.status(404).json({ 'message': "User not found" });
